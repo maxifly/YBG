fdb725935b0966c31993a110d32abcbaa5df7fb1
maxifly <maxifly@mail.ru>
2023-11-15 01:52:58 +0300

    fix

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 	"os"
 )
 
-//var tpl = template.Must(template.ParseFiles(files...))
+type BackupFileInfo struct {
+	Name       string
+	CreateDate string
+	Size       string
+	IsLocal    bool
+	IsRemote   bool
+}
+
+type BackupResponse struct {
+	BFiles []BackupFileInfo
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	files := []string{
@@ -29,7 +39,15 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// Затем мы используем метод Execute() для записи содержимого
 	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
 	// возможность отправки динамических данных в шаблон.
-	err = ts.Execute(w, nil)
+
+	bFiles := make([]BackupFileInfo, 0, 0)
+
+	bFiles = append(bFiles, BackupFileInfo{Name: "test1", CreateDate: "01.01", Size: "125", IsLocal: true, IsRemote: true})
+	bFiles = append(bFiles, BackupFileInfo{Name: "test2", CreateDate: "02.02", Size: "125", IsLocal: true, IsRemote: true})
+
+	data := BackupResponse{BFiles: bFiles}
+
+	err = ts.Execute(w, data)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -43,10 +61,12 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", indexHandler)
 
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	log.Printf("Запуск веб-сервера на http://127.0.0.1:%s", port)
 	err := http.ListenAndServe(":"+port, mux)
 	log.Fatal(err)
 }
