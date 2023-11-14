9cf1bc79a3e207d9096332c1757195bf8a4868ba
maxifly <maxifly@mail.ru>
2023-11-15 01:05:43 +0300

    fix

--- main.go
+++ main.go
@@ -2,27 +2,51 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 )
 
-var tpl = template.Must(template.ParseFiles("index.html"))
+//var tpl = template.Must(template.ParseFiles(files...))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+	files := []string{
+		"./ui/html/index.html",
+		"./ui/html/base.html",
+	}
+
+	// Используем функцию template.ParseFiles() для чтения файлов шаблона.
+	// Если возникла ошибка, мы запишем детальное сообщение ошибки и
+	// используя функцию http.Error() мы отправим пользователю
+	// ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Затем мы используем метод Execute() для записи содержимого
+	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// возможность отправки динамических данных в шаблон.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = "4000"
 	}
 
 	mux := http.NewServeMux()
 
-	fs := http.FileServer(http.Dir("assets"))
-	mux.Handle("/assets/", http.StripPrefix("/assets", fs))
-
 	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":"+port, mux)
+	log.Fatal(err)
 }
