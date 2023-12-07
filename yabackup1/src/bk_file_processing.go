6177ce3e32fe0bff4d2b87b686dda82ea8b1485e
maxifly <maxifly@mail.ru>
2023-12-08 01:44:10 +0300

    parse local tar file

--- yabackup1/src/bk_file_processing.go
+++ yabackup1/src/bk_file_processing.go
@@ -5,11 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/fs"
-	"io/ioutil"
-	"log"
 	"os"
-	"path"
+	"path/filepath"
 )
 
 type BackupFileInfo struct {
@@ -25,14 +22,19 @@ type LocalBackupFile struct {
 	Name       string
 	CreateDate string
 	Path       string
-	Size       string
+	Size       int64
 }
 
 func GetFilesInfo(application *Application) ([]BackupFileInfo, error) {
 	application.debugLog.Println("Start get files")
 
 	getRemoteFiles(application)
+	err, localFiles := getLocalBackupFiles(application)
+	if err != nil {
+		return nil, err
+	}
 
+	application.debugLog.Printf("Local files: %+v", localFiles)
 	//TODO Create
 	bFiles := make([]BackupFileInfo, 0, 0)
 
@@ -42,19 +44,43 @@ func GetFilesInfo(application *Application) ([]BackupFileInfo, error) {
 	return bFiles, nil
 }
 
-func getLocalBacupFiles(app *Application) (error, map[string] LocalBackupFile) {
+func getLocalBackupFiles(app *Application) (error, map[string]LocalBackupFile) {
 
 	entries, err := os.ReadDir(BACKUP_PATH)
 	if err != nil {
 		app.errorLog.Printf("Unable to read backup %s. %v", BACKUP_PATH, err)
 		return fmt.Errorf("error when read local backups"), nil
 	}
-	infos := make([]fs.FileInfo, 0, len(entries))
+	result := make(map[string]LocalBackupFile)
 	for _, entry := range entries {
+		app.debugLog.Printf("entry %+v", entry)
 		info, err := entry.Info()
-		if err != nil { ... }
-		infos = append(infos, info)
+		if err != nil {
+			app.errorLog.Printf("Error read file info %v", err)
+			continue
+		}
+		app.debugLog.Printf("info: %+v", info)
+
+		if info.IsDir() {
+			continue
+		}
+
+		filePath := filepath.Join(BACKUP_PATH, info.Name())
+		app.debugLog.Printf("Read %s", filePath)
+		archInfo, err := extractArchInfo(app, filePath)
+		if err != nil {
+			app.errorLog.Printf("Error extract slug from %s %v", info.Name(), err)
+			continue
+		}
+
+		result[archInfo.Slug] = LocalBackupFile{Slug: archInfo.Slug,
+			Name:       info.Name(),
+			Path:       filePath,
+			Size:       info.Size(),
+			CreateDate: info.ModTime().String(),
+		}
 	}
+	return nil, result
 
 	//files, err := ioutil.ReadDir(".")
 	//if err != nil {
@@ -66,13 +92,23 @@ func getLocalBacupFiles(app *Application) (error, map[string] LocalBackupFile) {
 	//}
 }
 
-func extract(tarfile string) {
+type BackupArchInfo struct {
+	Slug string
+	Name string
+}
+
+func extractArchInfo(app *Application, tarfile string) (*BackupArchInfo, error) {
 	reader, err := os.Open(tarfile)
 	if err != nil {
-		fmt.Printf("ERROR: cannot read tar file, error=[%v]\n", err)
-		return
+		return nil, fmt.Errorf("ERROR: cannot read tar file, error=[%v]\n", err)
 	}
-	defer reader.Close()
+
+	defer func(reader *os.File) {
+		err := reader.Close()
+		if err != nil {
+			app.errorLog.Printf("Can not close reader, error=[%v]", err)
+		}
+	}(reader)
 
 	tarReader := tar.NewReader(reader)
 	for {
@@ -80,41 +116,34 @@ func extract(tarfile string) {
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			fmt.Printf("ERROR: cannot read tar file, error=[%v]\n", err)
-			return
+			return nil, fmt.Errorf("cannot read tar file, error=[%v]", err)
 		}
 
 		j, err := json.Marshal(header)
 		if err != nil {
-			fmt.Printf("ERROR: cannot parse header, error=[%v]\n", err)
-			return
+			return nil, fmt.Errorf("cannot parse header, error=[%v]", err)
 		}
-		fmt.Printf("header=%s\n", string(j))
+		app.infoLog.Printf("header=%s\n", string(j))
 
 		info := header.FileInfo()
-		if info.IsDir() {
-			if err = os.MkdirAll(header.Name, 0755); err != nil {
-				fmt.Printf("ERROR: cannot mkdir file, error=[%v]\n", err)
-				return
-			}
+		if info.IsDir() || info.Name() != "backup.json" {
+			continue
 		} else {
-			if err = os.MkdirAll(path.Dir(header.Name), 0755); err != nil {
-				fmt.Printf("ERROR: cannot file mkdir file, error=[%v]\n", err)
-				return
+			var data BackupArchInfo
+			plan, err := io.ReadAll(tarReader)
+
+			if err != nil {
+				return nil, fmt.Errorf("cannot read backup info, error=[%v]", err)
+
 			}
 
-			file, err := os.OpenFile(header.Name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+			err = json.Unmarshal(plan, &data)
 			if err != nil {
-				fmt.Printf("ERROR: cannot open file, error=[%v]\n", err)
-				return
-			}
-			defer file.Close()
+				return nil, fmt.Errorf("cannot parse backup info, error=[%v]", err)
 
-			_, err = io.Copy(file, tarReader)
-			if err != nil {
-				fmt.Printf("ERROR: cannot write file, error=[%v]\n", err)
-				return
 			}
+			return &data, nil
 		}
 	}
+	return nil, fmt.Errorf("backup info not found")
 }
